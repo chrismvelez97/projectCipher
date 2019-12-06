845e5c11ae8aeaf8bc2a891032a0cd4e7ddf7ffd
Christian Velez <chrismvelez97@gmail.com>
2019-12-06 15:45:08 -0800

    In this phase I tweaked the code to handle whitespace and upper case letters as well.  I also wrapped the statements in the Enduser file into a function, which was moved to the cipherFunctions file.  After that I changed the Enduser file to a while loop that continuosly asked for different messages to encrypt.  It takes a command exit that can exit out of the program.

--- EndUser.py
+++ EndUser.py
@@ -1,14 +1,24 @@
 import cipherFunctions as cipher
+'''This is the old way it was done but now
+    we wrapped our statements into a cipherFunctions
+    and then used a while loop to have the
+    program continue to hash different messages
+    until the command "exit" is entered.'''
 
+# message = input("Enter message here:\n")
+#
+# numList = cipher.messageToNumber(message)
+#
+# shift = cipher.shiftNumbers(numList)
+#
+# encryption = cipher.checkOutOfBounds(shift)
+#
+# encryption = cipher.shiftAsEncryption(encryption)
+#
+#
+# print("Your encrypted message is:\n{}".format(encryption))
+#
 message = input("Enter message here:\n")
-
-numList = cipher.messageToNumber(message)
-
-shift = cipher.shiftNumbers(numList)
-
-encryption = cipher.checkOutOfBounds(shift)
-
-encryption = cipher.shiftAsEncryption(encryption)
-
-
-print("Your encrypted message is:\n{}".format(encryption))
+while message != "exit":
+    cipher.displayCipher(message)
+    message = input("Enter message here:\n")
